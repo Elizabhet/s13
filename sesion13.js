f8bf122d1a30d68df30e70af337ee439f6629cd8
elizabhet <heniostroza@gmail.com>
2021-12-23 16:28:48 -0500

    archivo

---
+++ sesion13.js
@@ -0,0 +1,61 @@
+
+/*var nombre = "elizabhet";
+
+if(nombre.length> 6)
+{
+    console.log("tu nombre es muy largo!°o°");
+}
+var numero1 = 32
+var numero2 = 34
+
+var suma = numero1 + numero2;
+
+if(suma> 70 ){
+    console.log("Aprobaste")
+}else{
+    console.log("Desaprobaste")
+}
+
+var nota3 = 16
+var nota4 = 18
+var nota5 = 16
+
+var suma = nota3 + nota4 + nota5;
+var promedio = suma / 3
+
+if(promedio > 11){
+    console.log("aporbaste")
+}else{
+    console.log("Desaprobaste")
+}
+
+var estacion1 = "priMavera";
+
+var estacion2 = "verano";
+
+var estacion3 = "inviErno";
+
+var estacion4 = "otoño";
+
+if("primavera" == estacion1.toLowerCase)
+{
+    console.log("coinside la estacion")
+}else{
+    console.log("la estacion no coincide")
+}*/
+
+var clima = "nublado";
+switch (clima){
+    case 'llusioso':
+        console.log('recuerda llevar un paraguas.');
+        break;
+    case 'soleado':
+        console.log('viste ligero.');
+        break;
+    case 'nublado':
+        console.log('sal a la calle');
+        break;
+    default:
+        console.log('tipo de clima desconocido.');
+        break;
+}
